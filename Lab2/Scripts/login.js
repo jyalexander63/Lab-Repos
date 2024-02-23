00accc369af4fc431f2f7720e487806df8650787
jyalexander63 <156140974+jyalexander63@users.noreply.github.com>
2024-02-22 22:52:04 -0500

    xx

--- Lab2/Scripts/login.js
+++ Lab2/Scripts/login.js
@@ -8,9 +8,6 @@ $(document).ready(function() {
         var username = $('#username').val();
         var password = $('#password').val();
         
-        // For demonstration, let's assume the login is successful
-        // In a real scenario, you would validate the credentials with your backend
-        
         // Insert the username into the navbar
         var usernameListItem = '<li class="nav-item navbar-text">Welcome, ' + username + '</li>';
         $('#login-logout').before(usernameListItem);
